eff39d3e5a99b63ada10d3b053c7af50394f04b0
Maciej Sz <maciej.sz@gmail.com>
2015-03-11 14:06:47 +0100

    tests

--- tests/php-unit/src/bootstrap.php
+++ tests/php-unit/src/bootstrap.php
@@ -3,5 +3,5 @@
 error_reporting(E_ALL | E_STRICT);
 
 /** @var \Composer\Autoload\ClassLoader $Loader */
-$Loader = require_once __DIR__ . "/../../vendor/autoload.php";
+$Loader = require_once __DIR__ . "/../../../vendor/autoload.php";
 
