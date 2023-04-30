b055c1c8e979e124f589c7068b6e89aa8b83e05e
Maciej Szopinski <maciej.sz@gmail.com>
2023-04-30 22:06:24 +0200

    date format functions tests

--- src/Shared/Domain/DateFormatter/functions.php
+++ src/Shared/Domain/DateFormatter/functions.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace MaciejSz\Nbp\Shared\Domain\DateFormatter;
 
+use MaciejSz\Nbp\Shared\Domain\Exception\InvalidDateException;
+
 function first_day_of_month(int $year, int $month): string
 {
     return date('Y-m-d', strtotime("{$year}-{$month}-01"));
@@ -20,12 +22,16 @@ function last_day_of_month(int $year, int $month): string
 function month_range(int $year, int $month): array
 {
     return [
-        'startDate' => first_day_of_month($year, $month),
-        'endDate' => last_day_of_month($year, $month),
+        first_day_of_month($year, $month),
+        last_day_of_month($year, $month),
     ];
 }
 
 function format_ym(int $year, int $month): string
 {
-    return date('Y-m', strtotime("{$year}-{$month}"));
+    $timestamp = strtotime("{$year}-{$month}");
+    if (false === $timestamp) {
+        throw new InvalidDateException("Invalid year: {$year} and/or month: {$month}");
+    }
+    return date('Y-m', $timestamp);
 }
--- tests/unit/Shared/Domain/DateFormatterTest.php
+++ tests/unit/Shared/Domain/DateFormatterTest.php
@@ -4,14 +4,51 @@ declare(strict_types=1);
 
 namespace MaciejSz\Nbp\Test\Unit\Shared\Domain;
 
+use MaciejSz\Nbp\Shared\Domain\Exception\InvalidDateException;
 use PHPUnit\Framework\TestCase;
 use function MaciejSz\Nbp\Shared\Domain\DateFormatter\first_day_of_month;
+use function MaciejSz\Nbp\Shared\Domain\DateFormatter\format_ym;
+use function MaciejSz\Nbp\Shared\Domain\DateFormatter\last_day_of_month;
+use function MaciejSz\Nbp\Shared\Domain\DateFormatter\month_range;
 
 class DateFormatterTest extends TestCase
 {
-    public function testFirstDayOfMonth()
+    public function testFirstDayOfMonth(): void
     {
         $this->assertSame('2022-01-01', first_day_of_month(2022, 1));
         $this->assertSame('1234-12-01', first_day_of_month(1234, 12));
+    }
+
+    public function testLastDayOfMonth(): void
+    {
+        $this->assertSame('2023-03-31', last_day_of_month(2023, 3));
+        $this->assertSame('2020-02-29', last_day_of_month(2020, 2));
+    }
+
+    public function testMonthRange(): void
+    {
+        $this->assertSame(
+            ['2023-03-01', '2023-03-31'],
+            month_range(2023, 3)
+        );
+
+        $this->assertSame(
+            ['2020-02-01', '2020-02-29'],
+            month_range(2020, 2)
+        );
+    }
+
+    public function testFormatYm()
+    {
+        self::assertSame('2020-09', format_ym(2020, 9));
+        self::assertSame('1970-01', format_ym(1970, 1));
+    }
+
+    public function testFormatYmInvalid()
+    {
+        self::expectException(InvalidDateException::class);
+        self::expectExceptionMessage('Invalid year: 2020 and/or month: 13');
+
+        format_ym(2020, 13);
     }
 }
